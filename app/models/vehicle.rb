549ce3358a3e87376d12069a2a79198a1a151514
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-12-03 08:13:02 -0300

    add vehicle spec and factory

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Vehicle < ApplicationRecord
-  validates :id, presence: true
-  validates :id, uniqueness: { case_sensitive: false }
+  validates :vehicle_id, presence: true
+  validates :vehicle_id, uniqueness: { case_sensitive: false }
 
   has_many :waypoints, dependent: :destroy
 end
