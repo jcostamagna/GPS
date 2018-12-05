640ec7e9d17fc1571d0762e7f2f4258995328fb2
Jua Costamagna <juan.costamagna@mercadolibre.com>
2018-12-05 16:46:13 -0300

    Add waypoints

--- app/models/waypoint.rb
+++ app/models/waypoint.rb
@@ -2,8 +2,8 @@
 
 class Waypoint < ApplicationRecord
   validates :latitude, :longitude, :time, presence: true
-  validates :latitude, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 50 }
-  validates :longitude, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 50 }
+  validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 
   belongs_to :vehicle
 end
